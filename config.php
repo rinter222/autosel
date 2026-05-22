77304bbf1f2e1b42fa6401c10db5539b8794ce98
rinter222 <rin.muhametov@yandex.ru>
2026-05-22 10:44:44 +0500

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'mysql.web-prj.ru');
-define('DB_NAME', 'autosel');
-define('DB_USER', 'autosel');      // ← Замените на свой логин
-define('DB_PASS', 'Me47682$');    // ← Замените на свой пароль
-?>
