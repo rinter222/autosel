c82f20e81495eeafb2ef4089b998d706955a8e1c
rinter222 <rin.muhametov@yandex.ru>
2026-05-22 10:36:27 +0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'mysql.web-prj.ru');
+define('DB_NAME', 'autosel');
+define('DB_USER', 'autosel');      // ← Замените на свой логин
+define('DB_PASS', 'Me47682$');    // ← Замените на свой пароль
+?>
